1137398eb0a44c68d42ee90edfde4148e81465cf
isesword <112743751+isesword@users.noreply.github.com>
2024-03-07 17:34:09 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,8 +5,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://myblog.esword.cn/' },
+    { title: '我的微信公众号', url: 'https://Esword56.github.io/picx-images-hosting/blog-img/vx.26l82sxx4u.jpg' }
   ],
 
   // 英雄区左右侧组件颠倒位置
